704a9837fdf3c9feab4d856d9a5d6de47368d89f
NightBlood38 <tozser.szabi@gmail.com>
2024-05-09 02:54:35 +0200

    basic player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,38 +1,99 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{   
+{
+    public float moveDistance = 2.0f;
     public float xMin = -7.0f;
     public float xMax = 7.0f;
-    public float zMin = -10.0f; 
+    public float zMin = -10.0f;
     public float zMax = 4.0f;
+
     void Update()
     {
-        
-        Vector3 movement = new Vector3(0,0,0);
+        Vector3 movement = Vector3.zero;
 
-        if(Input.GetKeyDown(KeyCode.A) == true)
+
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            movement = new Vector3(-2, 0, 0);
+            movement.x = -moveDistance;
         }
-        else if(Input.GetKeyDown(KeyCode.D) == true)
+        else if (Input.GetKeyDown(KeyCode.D))
         {
-            movement = new Vector3(2, 0, 0);
+            movement.x = moveDistance;
         }
-        else if(Input.GetKeyDown(KeyCode.W) == true)
+        else if (Input.GetKeyDown(KeyCode.W))
         {
-            movement = new Vector3(0, 0, 2);
+            movement.z = moveDistance;
         }
-        else if(Input.GetKeyDown(KeyCode.S) == true)
+        else if (Input.GetKeyDown(KeyCode.S))
         {
-            movement = new Vector3(0, 0, -2);
+            movement.z = -moveDistance;
         }
+
         Vector3 newPosition = transform.position + movement;
 
-        newPosition.x = Mathf.Clamp(newPosition.x, xMin, xMax);
-        newPosition.z = Mathf.Clamp(newPosition.z, zMin, zMax);
+        bool isInTopLeftCorner = transform.position.x == xMin && transform.position.z == zMax;
+        bool isInTopRightCorner = transform.position.x == xMax && transform.position.z == zMax;
+        bool isInBottomLeftCorner = transform.position.x == xMin && transform.position.z == zMin;
+        bool isInBottomRightCorner = transform.position.x == xMax && transform.position.z == zMin;
+
+        if (isInTopLeftCorner)
+        {
+            if (movement.x > 0)
+            {
+                newPosition.x = Mathf.Clamp(newPosition.x, xMin, xMax);
+            }
+            if (movement.z < 0)
+            {
+                newPosition.z = Mathf.Clamp(newPosition.z, zMin, zMax);
+            }
+        }
+        else if (isInTopRightCorner)
+        {
+            if (movement.x < 0)
+            {
+                newPosition.x = Mathf.Clamp(newPosition.x, xMin, xMax);
+            }
+            if (movement.z < 0)
+            {
+                newPosition.z = Mathf.Clamp(newPosition.z, zMin, zMax);
+            }
+        }
+        else if (isInBottomLeftCorner)
+        {
+            if (movement.x > 0)
+            {
+                newPosition.x = Mathf.Clamp(newPosition.x, xMin, xMax);
+            }
+            if (movement.z > 0)
+            {
+                newPosition.z = Mathf.Clamp(newPosition.z, zMin, zMax);
+            }
+        }
+        else if (isInBottomRightCorner)
+        {
+            if (movement.x < 0)
+            {
+                newPosition.x = Mathf.Clamp(newPosition.x, xMin, xMax);
+            }
+            if (movement.z > 0)
+            {
+                newPosition.z = Mathf.Clamp(newPosition.z, zMin, zMax);
+            }
+        }
+        else
+        {
+            if (transform.position.z <= zMin || transform.position.z >= zMax)
+            {
+                newPosition.x = Mathf.Clamp(newPosition.x, xMin, xMax);
+                newPosition.z = Mathf.Clamp(transform.position.z, zMin, zMax);
+            }
+            else if (transform.position.x <= xMin || transform.position.x >= xMax)
+            {
+                newPosition.z = Mathf.Clamp(newPosition.z, zMin, zMax);
+                newPosition.x = Mathf.Clamp(transform.position.x, xMin, xMax);
+            }
+        }
 
         transform.position = newPosition;
     }
